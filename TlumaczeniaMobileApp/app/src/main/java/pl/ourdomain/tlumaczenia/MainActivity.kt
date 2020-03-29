b5f572543fa9cfeceee545b97ec7e03a65f5b903
DanielSporysz <daniel_spory@o2.pl>
2020-03-29 13:00:50 +0200

    Revert "commit before migration to AndroidX"
    This reverts commit 2fe53dd1

--- TlumaczeniaMobileApp/app/src/main/java/pl/ourdomain/tlumaczenia/MainActivity.kt
+++ TlumaczeniaMobileApp/app/src/main/java/pl/ourdomain/tlumaczenia/MainActivity.kt
@@ -2,9 +2,6 @@ package pl.ourdomain.tlumaczenia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toolbar
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import pl.ourdomain.tlumaczenia.R
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        val navController = findNavController(R.id.myNavHostFragment)
-//        val appBarConfiguration = AppBarConfiguration(navController.graph)
-//        findViewById<Toolbar>(R.id.toolbar)
-//            .setupWithNavController(navController, appBarConfiguration)
     }
 }
