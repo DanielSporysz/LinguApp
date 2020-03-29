2fe53dd11e1d7b13cdc0a6d78bcb7becc1d35f08
DanielSporysz <daniel_spory@o2.pl>
2020-03-29 12:39:52 +0200

    commit before migration to AndroidX

--- TlumaczeniaMobileApp/app/src/main/java/pl/ourdomain/tlumaczenia/MainActivity.kt
+++ TlumaczeniaMobileApp/app/src/main/java/pl/ourdomain/tlumaczenia/MainActivity.kt
@@ -2,6 +2,9 @@ package pl.ourdomain.tlumaczenia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import pl.ourdomain.tlumaczenia.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val navController = findNavController(R.id.myNavHostFragment)
+//        val appBarConfiguration = AppBarConfiguration(navController.graph)
+//        findViewById<Toolbar>(R.id.toolbar)
+//            .setupWithNavController(navController, appBarConfiguration)
     }
 }
