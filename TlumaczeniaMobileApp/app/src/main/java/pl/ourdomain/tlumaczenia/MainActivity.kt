805bcd9778c154ded1b1eb98fb317731f9cc2b75
DanielSporysz <daniel_spory@o2.pl>
2020-03-29 12:34:05 +0200

    Toolbar, AndroidX

--- TlumaczeniaMobileApp/app/src/main/java/pl/ourdomain/tlumaczenia/MainActivity.kt
+++ TlumaczeniaMobileApp/app/src/main/java/pl/ourdomain/tlumaczenia/MainActivity.kt
@@ -1,13 +1,16 @@
 package pl.ourdomain.tlumaczenia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import pl.ourdomain.tlumaczenia.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById<Toolbar>(R.id.my_toolbar)
+        setSupportActionBar(toolbar)
     }
 }
